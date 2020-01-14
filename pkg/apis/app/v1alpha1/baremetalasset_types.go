7d0398d3d413b6cef1fa8eb36ac6346a145566c3
Sudarshan Acharya <sudarshan.acharya@redhat.com>
2020-01-13 22:11:56 -0600

    Create a basic inventory data structure ie. BareMetalAssetSpec

--- pkg/apis/app/v1alpha1/baremetalasset_types.go
+++ pkg/apis/app/v1alpha1/baremetalasset_types.go
@@ -7,11 +7,52 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// BMCDetails contains the information necessary to communicate with
+// the bare metal controller module on host.
+type BMCDetails struct {
+
+	// Address holds the URL for accessing the controller on the
+	// network.
+	Address string `json:"address"`
+
+	// The name of the secret containing the BMC credentials (requires
+	// keys "username" and "password").
+	CredentialsName string `json:"credentialsName"`
+}
+
+// Role represents the role assigned to the asset
+type Role string
+
+const (
+	// MasterRole is the master role assigned to the asset
+	MasterRole Role = "master"
+
+	// WorkerRole is the worker role assigned to the asset
+	WorkerRole Role = "worker"
+)
+
 // BareMetalAssetSpec defines the desired state of BareMetalAsset
 type BareMetalAssetSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// How do we connect to the BMC?
+	BMC BMCDetails `json:"bmc,omitempty"`
+
+	// What is the name of the hardware profile for this host? It
+	// should only be necessary to set this when inspection cannot
+	// automatically determine the profile.
+	HardwareProfile string `json:"hardwareProfile,omitempty"`
+
+	// Which MAC address will PXE boot? This is optional for some
+	// types, but required for libvirt VMs driven by vbmc.
+	// +kubebuilder:validation:Pattern=`[0-9a-fA-F]{2}(:[0-9a-fA-F]{2}){5}`
+	BootMACAddress string `json:"bootMACAddress,omitempty"`
+
+	// Role holds the role of the asset
+	// +kubebuilder:validation:Enum=master;worker
+	Role Role `json:"role,omitempty"`
 }
 
 // BareMetalAssetStatus defines the observed state of BareMetalAsset
